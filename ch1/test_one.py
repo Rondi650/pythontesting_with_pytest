2d829aa578257a6aa6418d3310c65b07e5ec6ed6
Rondi650 <rondinellenunes123@gmail.com>
2026-06-13 19:07:28 -0300

    Add additional test cases for validation in test_one.py and test_two.py

--- ch1/test_one.py
+++ ch1/test_one.py
@@ -1,2 +1,11 @@
 def test_passing():
     assert (123) == (123)
+    
+def test_one_in_list():
+    assert 1 in [1,2,3]
+    
+def test_a_lt_b():
+    assert 'a' < 'b'
+    
+def test_rondi():
+    assert 'rondi' not in 'rondinelle'
--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,2 +1,8 @@
 def test_failing():
     assert (123) == (321)
+    
+    
+def test_comparando_contas():
+    conta1 = 1+2+3+4+5
+    conta2 = 5+4+3+2+1
+    assert conta1 == conta2

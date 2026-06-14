83c575d2ef3bcae568de077ed165369f011a391e
Rondi650 <rondinellenunes123@gmail.com>
2026-06-14 15:48:08 -0300

    Fix assertions in test_two.py and improve test cases for account and string comparisons

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,8 +1,14 @@
 def test_failing():
-    assert (123) == (321)
-    
-    
-def test_comparando_contas():
+    assert (1, 2, 3) == (3, 2, 1)
+
+
+def testcomparando_contas():
     conta1 = 1+2+3+4+5
-    conta2 = 5+4+3+2+1
+    conta2 = 5+4+3+2+1.1
     assert conta1 == conta2
+
+
+def test_comparando_strings():
+    string1 = 'python'
+    string2 = 'Python'
+    assert string1 == string2
